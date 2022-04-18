c7c06f17ea8bbd67a3efea78cb2961a632283121
xiaozhou <librazhoux@hotmail.com>
2022-04-18 17:53:35 +0800

    feat: add login page form rules

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import { useRouter } from "vue-router";
 import { initRouter } from "/@/router/utils";
 import { storageSession } from "/@/utils/storage";
+import { Lock, User } from "@element-plus/icons-vue";
+import type { FormInstance } from "element-plus";
 
 const router = useRouter();
 
@@ -10,6 +12,25 @@ const loginForm = reactive({
   username: "admin",
   password: "123456"
 });
+
+const formRules = {
+  username: [
+    {
+      required: true,
+      message: "请输入用户名",
+      trigger: "blur"
+    }
+  ],
+  password: [
+    {
+      required: true,
+      message: "请输入密码",
+      trigger: "blur"
+    }
+  ]
+};
+
+const loginFormRef = ref<FormInstance>();
 
 const motionInitial = {
   opacity: 0,
@@ -24,21 +45,31 @@ const motionEnter = delay => ({
   }
 });
 
-const onLogin = (): void => {
-  storageSession.setItem("info", {
-    username: "admin",
-    accessToken: "eyJhbGciOiJIUzUxMiJ9.test"
+const onLogin = (formEl: FormInstance | undefined): void => {
+  if (!formEl) return;
+  formEl.validate(valid => {
+    if (valid) {
+      storageSession.setItem("info", {
+        username: "admin",
+        accessToken: "eyJhbGciOiJIUzUxMiJ9.test"
+      });
+      initRouter("admin").then(() => {});
+      router.push("/");
+    }
   });
-  initRouter("admin").then(() => {});
-  router.push("/");
 };
 </script>
 
 <template>
   <div class="login-container">
-    <el-form :model="loginForm" class="login-form">
+    <el-form
+      ref="loginFormRef"
+      :model="loginForm"
+      :rules="formRules"
+      class="login-form"
+    >
       <h2 v-motion :initial="motionInitial" :enter="motionEnter(100)">
-        Admin Template
+        Pure Admin
       </h2>
       <el-form-item
         prop="username"
@@ -46,7 +77,11 @@ const onLogin = (): void => {
         :initial="motionInitial"
         :enter="motionEnter(200)"
       >
-        <el-input v-model="loginForm.username" autocomplete="off" />
+        <el-input
+          v-model="loginForm.username"
+          autocomplete="off"
+          :prefix-icon="User"
+        />
       </el-form-item>
       <el-form-item
         prop="password"
@@ -58,10 +93,14 @@ const onLogin = (): void => {
           v-model="loginForm.password"
           type="password"
           autocomplete="off"
+          :prefix-icon="Lock"
         />
       </el-form-item>
       <el-form-item v-motion :initial="motionInitial" :enter="motionEnter(400)">
-        <el-button style="width: 100%" type="primary" @click="onLogin"
+        <el-button
+          style="width: 100%"
+          type="primary"
+          @click="onLogin(loginFormRef)"
           >登 录</el-button
         >
       </el-form-item>
